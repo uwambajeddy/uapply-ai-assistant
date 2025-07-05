eacd0fa306a5e4e587fe3dcd24a0035e9b86089c
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2025-07-05 20:10:21 +0200

    Fix: the input component

--- components/assistant-ui/thread.tsx
+++ components/assistant-ui/thread.tsx
@@ -46,7 +46,7 @@ export const Thread: FC = () => {
           <div className="min-h-8 flex-grow pt-20" />
         </ThreadPrimitive.If>
 
-        <div className="sticky bottom-0 mt-3 flex w-full max-w-[var(--thread-max-width)] flex-col items-center justify-end rounded-t-lg bg-inherit pb-4 fixed">
+        <div className="fixed bottom-0 mt-3 flex w-full max-w-[var(--thread-max-width)] flex-col items-center justify-end rounded-t-lg bg-inherit pb-4 ">
           <ThreadScrollToBottom />
           <Composer />
         </div>
