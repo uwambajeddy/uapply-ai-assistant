fc49d2352eb077169722b17d7fd8223777ecc9da
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2025-07-05 19:39:00 +0200

    Fix: Update the prompt

--- components/assistant-ui/thread.tsx
+++ components/assistant-ui/thread.tsx
@@ -89,12 +89,12 @@ const ThreadWelcomeSuggestions: FC = () => {
     <div className="mt-3 flex w-full items-stretch justify-center gap-4">
       <ThreadPrimitive.Suggestion
         className="hover:bg-muted/80 flex max-w-sm grow basis-0 flex-col items-center justify-center rounded-lg border p-3 transition-colors ease-in"
-        prompt="How can I apply to African Leadership University?"
+        prompt="Hi"
         method="replace"
         autoSend
       >
         <span className="line-clamp-2 text-ellipsis text-sm font-semibold">
-          How can I apply to African Leadership University?
+          I want to apply to African Leadership University?
         </span>
       </ThreadPrimitive.Suggestion>
       <ThreadPrimitive.Suggestion
